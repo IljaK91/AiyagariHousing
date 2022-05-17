cb99984cc1cb2eec93346a43f7693c0cea71376c
Ilja Kantorovitch <iljakant@gmail.com>
2022-05-17 15:54:09 +0200

    Adjust import and functions that are exported

--- src/AiyagariHousing.jl
+++ src/AiyagariHousing.jl
@@ -1,12 +1,11 @@
 module AiyagariHousing
 
-using Parameters, QuantEcon, Distributions, NLopt, CompEcon
-
-import LinearAlgebra.mul!
+using Parameters, QuantEcon, Distributions, CompEcon, Printf, JuMP, Setfield, AutomaticDocstrings
+import LinearAlgebra.mul!, Ipopt
 
 include("Types.jl")
 include("Functions.jl")
 
-export Pars, u, u_ind, Psi, myfunc, myconstraint, @unpack_Pars, lom_housing, vmax, myfunc2, hello_Irene
-    #lom_assets, lom_housing, find_EV, find_c, Val_of_c, EV_of_s, find_c_constr
+export Pars, u, u_ind, Psi, @unpack_Pars, lom_housing, myfunc2, solve_model, setup_Q, Prob_a_next, calc_step, delta_low, delta_high, marketclearing_housing
+#lom_assets, lom_housing, find_EV, find_c, Val_of_c, EV_of_s, find_c_constr
 end # module
