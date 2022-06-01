072f9ecc6d97e4c4fc9414490c54176c6268cb47
Ilja Kantorovitch <iljakant@gmail.com>
2022-06-01 12:02:48 +0200

    Export additional functions

--- src/AiyagariHousing.jl
+++ src/AiyagariHousing.jl
@@ -6,6 +6,6 @@ import LinearAlgebra.mul!, Ipopt
 include("Types.jl")
 include("Functions.jl")
 
-export Pars, u, u_ind, Psi, @unpack_Pars, lom_housing, myfunc2, solve_model, setup_Q, Prob_a_next, calc_step, delta_low, delta_high, marketclearing_housing, find_rental_rate, solve_GE
+export Pars, u, u_ind, Psi, @unpack_Pars, lom_housing, myfunc2, solve_model, setup_Q, Prob_a_next, calc_step, delta_low, delta_high, marketclearing_housing, find_rental_rate, solve_GE, stst_distr, comp_statics, risk_premium
 #lom_assets, lom_housing, find_EV, find_c, Val_of_c, EV_of_s, find_c_constr
 end # module
